438f084ae72264c6d0943cb0650974c823cf1ba8
Tonya Camille <tsims25@gmail.com>
2022-03-03 02:25:01 -0600

    fix issue

--- stream/stream/settings.py
+++ stream/stream/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-95xf#1)zwf4h8kxhiqa)a8lda3znb+k-djg^xa_=d&l5))l+0t'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
